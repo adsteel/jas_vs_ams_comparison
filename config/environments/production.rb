0aac7bc3de764f2a8b7d41c031d6b2d03d5e4d68
Adam Steel <adamgsteel@gmail.com>
2020-07-24 15:47:46 -0600

    rename project

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -50,7 +50,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "fast_jsonapi_comparison_production"
+  # config.active_job.queue_name_prefix = "jas_vs_ams_comparison_production"
 
   config.action_mailer.perform_caching = false
 
